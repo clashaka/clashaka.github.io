2178e66c1b89122bd14d43f0119730d057aae34d
clashaka <clashaka@outlook.com>
2024-10-30 11:02:19 +0800

    Add new content

--- .vitepress/theme/Footer.js
+++ .vitepress/theme/Footer.js
@@ -7,6 +7,6 @@ for (let i = 0; i < link.length; i++) {
 }
 
 export default {
-    message: `<a href="/about">关于我们</a>&nbsp;|&nbsp;<a href="/contact">联系我们</a>&nbsp;|&nbsp;<a href="/link-exchange">友情链接</a>&nbsp;|&nbsp;<a href="/privacy-policy">隐私权政策</a>`,
-    copyright: `© ${new Date().getFullYear()} 一个朋友<br /><small style="display:flex;justify-content:center;gap:8px;">${linkString}</small>`
+    message: `<small><a href="/about">关于我们</a>&nbsp;|&nbsp;<a href="/contact">联系我们</a>&nbsp;|&nbsp;<a href="/link-exchange">友情链接</a>&nbsp;|&nbsp;<a href="/privacy-policy">隐私权政策</a></small>`,
+    copyright: `<small>© ${new Date().getFullYear()} 一个朋友</small><br /><small style="display:flex;justify-content:center;gap:8px;">${linkString}</small>`
 }
