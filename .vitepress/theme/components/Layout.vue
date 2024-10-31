6419332fc3b599a23e3e5dc51e13fd1ffb90e40d
clashaka <clashaka@outlook.com>
2024-10-31 12:33:07 +0800

    Add new content

--- .vitepress/theme/components/Layout.vue
+++ .vitepress/theme/components/Layout.vue
@@ -17,18 +17,18 @@ const { Layout } = DefaultTheme
             <div class="before"></div>
             <a href="/contact" class="nav VPBadge info">成为赞助商</a>
         </template>
-        <template #aside-outline-before>
+        <!-- <template #aside-outline-before>
             <a href="https://fast999.jsjsq.cc/" target="_blank">
                 <img src="/images/sponsor/jisu/2024101600.webp" alt="极速VPN 注册立即获得免费时长30分钟，绑定我的邀请码(952550)额外赠送免费时长30分钟" title="点击访问" />
             </a>
             <br>
-        </template>
-        <template #doc-footer-before>
+        </template> -->
+        <!-- <template #doc-footer-before>
             <a href="https://fast999.jsjsq.cc/" target="_blank">
                 <img src="/images/sponsor/jisu/2024101601.webp" alt="极速VPN 注册立即获得免费时长30分钟，绑定我的邀请码(952550)额外赠送免费时长30分钟" title="点击访问" />
             </a>
             <br>
-        </template>
+        </template> -->
     </Layout>
 </template>
 
