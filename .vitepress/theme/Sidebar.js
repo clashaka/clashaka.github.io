2eff63c242f9f844e3c317de6431716daeb4a241
clashaka <clashaka@outlook.com>
2024-12-06 11:35:20 +0800

    Add new content

--- .vitepress/theme/Sidebar.js
+++ .vitepress/theme/Sidebar.js
@@ -4,6 +4,7 @@ export default {
             text: '2024 年',
             items: [
                 { text: '12 月', link: '/vpn/2024/12' },
+                { text: '11 月', link: '/vpn/2024/11' },
                 { text: '10 月', link: '/vpn/2024/10' },
                 { text: '9 月', link: '/vpn/2024/09' },
                 { text: '8 月', link: '/vpn/2024/08' },
