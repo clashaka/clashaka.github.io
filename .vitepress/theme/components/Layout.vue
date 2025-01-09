f95016ccd63c989b94427bccbcdd50c54d27373a
clashaka <clashaka@outlook.com>
2025-01-09 11:36:20 +0800

    Add new content

--- .vitepress/theme/components/Layout.vue
+++ .vitepress/theme/components/Layout.vue
@@ -8,13 +8,6 @@ const { Layout } = DefaultTheme
     <Layout>
         <template #sidebar-nav-after>
             <!-- <a class="sponsor-sidebar" href="/contact">成为赞助商</a> -->
-            <a href="https://ber1.fnjs7.xyz/" target="_blank" rel="noreferrer nofollow">
-                <div class="gg-container">          
-                    <img src="/images/sponsor/feiniao/20241209.gif" alt="飞鸟加速器vpn推荐" title="点击访问" />
-                    <span class="gg-text">广告</span>
-                </div>
-            </a>
-            <br>
             <a href="https://mly01.miaolianyun.my/#/register?code=EeOKOE2v" target="_blank" rel="noreferrer nofollow">
                 <div class="gg-container">
                     <img src="/images/sponsor/miaolianyun/20241212.webp" alt="秒连云机场推荐" title="点击访问" />
