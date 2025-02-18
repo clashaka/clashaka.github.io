a263f66df53cab6ad09b82e9f1843b6f5fb86aa9
clashaka <clashaka@outlook.com>
2025-02-18 10:04:54 +0800

    Add new content

--- .vitepress/theme/components/Layout.vue
+++ .vitepress/theme/components/Layout.vue
@@ -9,7 +9,7 @@ const { Layout } = DefaultTheme
         <template #sidebar-nav-after>
             <a href="https://yunfan-a.uuvpn.xyz/#/register?code=7j3dYEtw" target="_blank" rel="noreferrer nofollow">
                 <div class="gg-container">
-                    <img src="/images/sponsor/vpnong/20250118.webp" alt="哆啦A梦VPN机场推荐" />
+                    <img src="/images/sponsor/vpnong/20250218.webp" alt="哆啦A梦VPN机场推荐" />
                     <span class="gg-text">广告</span>
                     <!-- <span class="re-text">前往体验</span> -->
                 </div>
