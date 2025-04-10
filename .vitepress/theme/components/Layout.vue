b2d5dc6e2f9f82c48cc8cd4289019a6c97251d3a
clashaka <clashaka@outlook.com>
2025-04-10 22:31:21 +0800

    Add new content

--- .vitepress/theme/components/Layout.vue
+++ .vitepress/theme/components/Layout.vue
@@ -11,7 +11,7 @@ const { Layout } = DefaultTheme
                 rel="noreferrer nofollow">
                 <div class="gg-container">
                     <img src="/images/sponsor/vpnong/20250218.webp" alt="哆啦A梦VPN机场推荐" />
-                    <span class="gg-text">推荐</span>
+                    <span class="gg-text">广告</span>
                     <!-- <span class="re-text">前往体验</span> -->
                 </div>
             </a>
@@ -23,7 +23,7 @@ const { Layout } = DefaultTheme
             <a href="https://superbiu.com/#/register?code=KH6xv0ou" target="_blank" rel="noreferrer nofollow">
                 <div class="gg-container">
                     <img src="/images/sponsor/superbiu/20250111.gif" alt="superbiu机场推荐" />
-                    <span class="gg-text">推荐</span>
+                    <span class="gg-text">广告</span>
                 </div>
             </a>
             <div class="msg">
@@ -38,7 +38,7 @@ const { Layout } = DefaultTheme
                         rel="noreferrer nofollow">
                         <div class="gg-container">
                             <img src="/images/sponsor/candytally/20250228.webp" class="testimg" alt="糖果云机场推荐" />
-                            <span class="gg-text">推荐</span>
+                            <span class="gg-text">广告</span>
                             <div class="msg">
                                 <a href="/vpn/paid#3">更多信息</a>
                             </div>
@@ -55,7 +55,7 @@ const { Layout } = DefaultTheme
                         rel="noreferrer nofollow">
                         <div class="gg-container">
                             <img src="/images/sponsor/candytally/20250228.webp" alt="糖果云机场推荐" />
-                            <span class="gg-text">推荐</span>
+                            <span class="gg-text">广告</span>
                             <div class="msg">
                                 <a href="/vpn/paid#3">更多信息</a>
                             </div>
