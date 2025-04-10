3ade3a5bdbf0a714d5842306afcf0783043dbe27
clashaka <clashaka@outlook.com>
2025-04-10 13:20:58 +0800

    Add new content

--- .vitepress/theme/components/Card.vue
+++ .vitepress/theme/components/Card.vue
@@ -1,7 +1,6 @@
 <script setup>
 defineProps({
     href: String,
-    target: String,
     image: String,
     title: String,
     details: String
@@ -9,102 +8,57 @@ defineProps({
 </script>
 
 <template>
-    <div class="card">
-        <a :href="href" :target="target" rel="noreferrer nofollow">
-            <article class="box">
-                <div class="logo">
-                    <div class="image-bg">
-                        <img class="image" :src="image" :alt="title">
+    <div data-v-f5090ebe="" class="item grid-3">
+        <a data-v-549c013f="" data-v-f5090ebe="" class="VPLink link no-icon VPFeature" :href="href" target="_blank"
+            :title="details">
+            <article data-v-549c013f="" class="box">
+                <div class="top">
+                    <div data-v-549c013f="" class="icon">
+                        <img :src="image" :alt="title" width="32" height="32">
                     </div>
-                    <h3 class="title">{{ title }}</h3>
+                    <h3 data-v-549c013f="" class="title">{{ title }}</h3>
                 </div>
-                <p class="details">{{ details }}</p>
+                <p data-v-549c013f="" class="details">{{ details }}</p>
             </article>
         </a>
     </div>
 </template>
 
 <style scoped>
-.card {
-    display: block;
-    border: 1px solid var(--vp-c-bg-soft);
-    border-radius: 12px;
-    height: 100%;
-    background-color: var(--vp-c-bg-soft);
-    transition: border-color 0.25s, background-color 0.25s;
-}
-
-.card:hover {
-    border-color: var(--vp-c-brand-1);
-}
-
-a,
-a:hover {
+a {
     color: inherit;
     text-decoration: inherit;
+    touch-action: manipulation;
 }
 
-:is(.vp-external-link-icon, .vp-doc a[href*='://'], .vp-doc a[target='_blank']):not(.no-icon)::after {
-    display: none;
+a:hover {
+    color: inherit;
+}
+
+h3,
+p {
+    overflow-wrap: break-word;
+    margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.item {
+    padding: 0;
 }
 
 .box {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
     padding: 8px;
-    height: 100%;
 }
 
-.logo {
+.top {
     display: flex;
     align-items: center;
     gap: 8px;
 }
 
-.image-bg {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 8px;
-    background-color: var(--vp-c-default-soft);
-    width: 48px;
-    height: 48px;
-    transition: background-color 0.25s;
-}
-
-.image {
-    width: 32px;
-}
-
-.title {
-    line-height: 24px;
-    font-size: 16px;
-    font-weight: 600;
-}
-
-.vp-doc h3 {
-    margin: 0;
-    border-top: 0;
-    padding-top: 0;
-    line-height: 0;
-}
-
-.details {
-    flex-grow: 1;
-    line-height: 24px;
-    font-size: 14px;
-    font-weight: 500;
-    color: var(--vp-c-text-2);
-}
-
-.vp-doc p {
-    margin: 0;
+.icon {
+    margin-bottom: 0;
 }
 </style>
-
-<!-- 示例：
-<div class="items-card">
-    <card href="/games/2024/05.html" target="_blank" image="/images/logo.webp" title="免费游戏" details="领取免费游戏，享受适度休闲。"/>
-</div>
--->
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import Layout from './components/Layout.vue'
 import Card from './components/Card.vue'
-import Card2 from './components/Card2.vue'
 import './custom.css'
 
 /** @type {import('vitepress').Theme} */
@@ -10,6 +9,5 @@ export default {
     Layout,
     enhanceApp({ app }) {
         app.component('Card', Card)
-        app.component('Card2', Card2)
     }
 }
