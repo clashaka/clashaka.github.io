3979be367b13622eb28431e869d311bff7e0dc85
clashaka <clashaka@outlook.com>
2025-04-18 23:31:43 +0800

    Add new content

--- .vitepress/theme/Sidebar.js
+++ .vitepress/theme/Sidebar.js
@@ -1,25 +1,36 @@
 export default {
     '/vpn/': [
         {
-            text: '2025 年',
+            text: '免费机场',
             items: [
-                { text: '4 月', link: '/vpn/2025/04' },
-                { text: '3 月', link: '/vpn/2025/03' },
-                { text: '2 月', link: '/vpn/2025/02' },
-                { text: '1 月', link: '/vpn/2025/01' }
+                {
+                    text: '2025 年',
+                    items: [
+                        { text: '4 月', link: '/vpn/2025/04' },
+                        { text: '3 月', link: '/vpn/2025/03' },
+                        { text: '2 月', link: '/vpn/2025/02' },
+                        { text: '1 月', link: '/vpn/2025/01' }
+                    ]
+                },
+                {
+                    text: '2024 年',
+                    collapsed: true,
+                    items: [
+                        { text: '12 月', link: '/vpn/2024/12' },
+                        { text: '11 月', link: '/vpn/2024/11' },
+                        { text: '10 月', link: '/vpn/2024/10' },
+                        { text: '9 月', link: '/vpn/2024/09' },
+                        { text: '8 月', link: '/vpn/2024/08' },
+                        { text: '7 月', link: '/vpn/2024/07' },
+                        { text: '6 月', link: '/vpn/2024/06' }
+                    ]
+                }
             ]
         },
         {
-            text: '2024 年',
-            collapsed: true,
+            text: '付费机场',
             items: [
-                { text: '12 月', link: '/vpn/2024/12' },
-                { text: '11 月', link: '/vpn/2024/11' },
-                { text: '10 月', link: '/vpn/2024/10' },
-                { text: '9 月', link: '/vpn/2024/09' },
-                { text: '8 月', link: '/vpn/2024/08' },
-                { text: '7 月', link: '/vpn/2024/07' },
-                { text: '6 月', link: '/vpn/2024/06' }
+                { text: new Date().getFullYear() + ' 年', link: '/vpn/paid' },
             ]
         }
     ]
